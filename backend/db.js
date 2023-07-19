1d53a833a4ad38dbb6507edd352741466ed369aa
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2023-07-20 00:06:03 +0800

    make db local

---
+++ backend/db.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+let dbConnection
+
+module.exports = {
+  connectToDb: (cb) => {
+    mongoose
+      .connect(process.env.MONGODB_URI + process.env.DB_NAME, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }) 
+      .then(() => {
+        console.log('Connected to MongoDB')
+        dbConnection = mongoose.connection
+        return cb();
+      })
+      .catch((err) => {
+        console.error(err)
+        return cb(err)
+      });
+  },
+  getDb: () => dbConnection
+};
--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,33 @@
-require('dotenv').config()
-
-const express = require('express')
+const {connectToDb, getDb} = require('./db')
 const mongoose = require('mongoose')
-const establishmentRoutes = require('./controller/routes/establishments')
+const express = require('express')
+const establishmentRoutes = require('./controller/routes/establishment')
 
-//app
+//app 
 const app = express() 
 
-//connect to db
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        app.listen(process.env.PORT, () => {
+connectToDb((err) => {
+    if (!err) {
+        app.listen(3000, () => {
             console.log('listening to 3000')
         })
-        
-    })
-    .catch((error) => console.log(error))
-
+        db = getDb()
+    }
+}) 
+             
 //view engine
 app.set('view engine', 'ejs')
 
 //middleware
-// app.use(express.json()) //to access req body
+app.use(express.json()) //to access req body
 app.use(express.static('public'))
 
-//routes
+//routes 
 app.get('/', (req, res) => {
-    res.render('index')
-})
+    res.redirect('/establishments')
+})  
+ 
+app.use('/establishments', establishmentRoutes) 
 
-app.use('/establishments', establishmentRoutes)
 
-
+   
