4df4dcebb2152bb80bcbc901224b965e1a0cdb18
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2023-07-23 12:17:03 +0800

    handle form validation

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,10 +1,17 @@
 const User = require('../model/User')
-const path = require('path')
 const jwt = require('jsonwebtoken')
 
 const handleErrors = (err) => {
-    console.log(err.message, err.code)
-}
+    let errors = { username: '', password: '' }
+    
+    if (err.message.includes('username')) {
+        errors.username = err.message
+    } else {
+        errors.password = err.message
+    }
+
+    return errors;
+  }
 
 //ayusin ung logic later
 const maxAge = 3 * 24 * 60 * 60
@@ -13,42 +20,32 @@ const createToken = (_id) => {
 }
 
 const login = async (req, res) => {
-    const currUser = req.body
+    // const currUser = req.body
+    const {username , password} = req.body
 
     try {
-        const user = await User.login(currUser)
+        const user = await User.login(username, password)
         const token = createToken(user._id)
         res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge})
-
-        const redirect = '/user/'+ user.username
-        res.status(200).redirect(redirect)
+        res.status(200).json({ user: user._id })
     } catch (error) {
-        handleErrors(error)
-        res.status(400).json({err: error.message})
-    }
+        const errors = handleErrors(error)
+        res.status(400).json({ errors })
+    } 
 } 
 
 const signup = async (req, res) => {
-    const currUser = req.body
-
-    try {
-        filePath = req.file.path
-        currUser.pfp = path.relative('public', filePath)
-    } catch(error) {
-        currUser.pfp = '/images/pfp/unknown.jpg'
-    }
-  
+    const {username , password, description, pfp} = req.body
+    
     try{
-        const user = await User.signup(currUser)
-        
+        const user = await User.signup(username , password, description, pfp)
         const token = createToken(user._id)
         res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge})
 
-        const redirect = '/user/'+ currUser["username-reg"]
-        res.status(200).redirect(redirect)
-    } catch (error) {
-        handleErrors(error)
-        res.status(400).json({err: error.message})
+        res.status(200).json({ user: user._id })
+    } catch (error) {    
+        const errors = handleErrors(error)
+        res.status(400).json({ errors })
     }
 }
 
@@ -61,7 +58,7 @@ const showLogin = (req, res) => {
 }
 
 const showSignup = (req, res) => {
-    res.render('signup')
+    res.render('signup', {error: null})
 }
 
 const logout = (req, res) => {
