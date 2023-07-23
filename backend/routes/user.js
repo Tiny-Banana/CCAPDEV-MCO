dd0b21063a1b24a66ea8edf0ffeb2581860ec262
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2023-07-23 12:17:51 +0800

    remove multer

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,27 +1,15 @@
 const express = require('express')
 const {login, signup, getUser, showLogin, showSignup, logout} = require('../controller/userController')
-const multer = require('multer')
-const path = require('path')
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/images/pfp')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-})
-
-const upload = multer({storage: storage})
 
 const router = express.Router()
 
 router.get('/login', showLogin)
 router.post('/login', login)
 router.get('/signup', showSignup)
-router.post('/signup', upload.single('file-reg'), signup)
+router.post('/signup', signup)
 router.get('/logout', logout)
 router.get('/:username', getUser)
 
-
+ 
 module.exports = router
